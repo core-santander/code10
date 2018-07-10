50026e4de3b9d371887d7ac7cdc1616d00840e35
core-santander <pugipedro@gmail.com>
2018-07-10 12:30:07 +0200

    ct

--- src/app/jugadores/jugadores.component.ts
+++ src/app/jugadores/jugadores.component.ts
@@ -10,14 +10,19 @@ export class JugadoresComponent implements OnInit {
 
   @Input() jugadores;
   @Output() canasta: EventEmitter<any> = new EventEmitter();
+  topJugadores: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.topJugadores = this.jugadores.concat();
   }
 
   setCanasta(puntos, nombre){
     this.canasta.emit({puntos, nombre});
+    this.topJugadores.sort((a, b)=>{
+      return (b.puntos - a.puntos);
+    });
   }
 
 }
